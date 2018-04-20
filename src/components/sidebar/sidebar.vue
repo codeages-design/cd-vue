a1a82ee25bb5a5640b2dd5bb460515bf1d306c34
ketuzhong <ketuzhong@yeah.net>
2018-04-20 16:06:25 +0800

    add sidebar component

---
+++ src/components/sidebar/sidebar.vue
@@ -0,0 +1,45 @@
+<template>
+  <aside :class="classes">
+    <div :class="`${prefixClass}__header`">
+      <div :class="`${prefixClass}__logo`">
+        <span v-if="title">
+          {{ this.title }}
+        </span>
+        <span v-else>
+          <slot name="logo"></slot>
+        </span>
+      </div>
+    </div>
+    <div :class="`${prefixClass}__body`">
+      <slot></slot>
+    </div>
+  </aside>
+</template>
+
+<script>
+import { prefix } from '../../utils';
+const prefixClass = `${prefix}-sidebar`;
+
+export default {
+  name: prefixClass,
+  props: [
+    'isCollapse',
+    'title'
+  ],
+  data() {
+    return {
+      prefixClass,
+    }
+  },
+  computed: {
+    classes() {
+      return [
+        prefixClass,
+        this.isCollapse ? `${prefixClass}--collapse` : ''
+      ]
+    }
+  }
+}
+</script>
+
+
